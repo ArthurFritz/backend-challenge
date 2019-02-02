74bb8670f15ccf0502a2ce2f7cd425ed9ec29bf7
Arthur Fritz Santiago <arthurfritzz@gmail.com>
2019-02-02 07:57:08 -0200

    Creation of modules this project

---
+++ documentation-service/src/main/java/com/invillia/acme/documentationservice/DocumentationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.invillia.acme.documentationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentationServiceApplication.class, args);
+    }
+
+}
+
---
+++ router-service/src/test/java/com/invillia/acme/routerservice/RouterServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.invillia.acme.routerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RouterServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
